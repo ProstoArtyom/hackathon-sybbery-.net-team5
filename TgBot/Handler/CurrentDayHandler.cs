38dcfb899f4f85eadd48d9b9dd7a007a2fb32c70
Kakelys <alexglobal1459@gmail.com>
2024-02-03 16:49:18 +0300

    get current code from api

--- TgBot/Handler/CurrentDayHandler.cs
+++ TgBot/Handler/CurrentDayHandler.cs
@@ -1,5 +1,7 @@
 using System.Security.Cryptography.X509Certificates;
+using BankAPI.Services;
 using Telegram.Bot;
+using Telegram.Bot.Requests;
 using Telegram.Bot.Types;
 using TgBot.Utils;
 
@@ -7,12 +9,32 @@ namespace TgBot.Handler
 {
     public class CurrentDayHandler
     {
+
+        private static Dictionary<string, int> currs = new()
+        {
+            {"USD", 145},
+            {"EUR", 19},
+            {"GBP", 143},
+            {"JPY", 67},
+        };
+
         public static async Task<bool> HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
         {
             var storedClient = LastActionStorage.Storage[update.Message.Chat.Id];
+            decimal curs = 0;
+            switch(storedClient.Data.BankName)
+            {
+                case "АльфаБанк":
+                    curs = (await GetInfo.GetRanks()).FirstOrDefault()?.SellRate ?? 0;
+                    break;
+                case "НБРБ":
+                    curs = (decimal)(await GetInfo.GetBBRates(currs[storedClient.Data.Currency])).Cur_OfficialRate;
+                    break;
+            }
+            
 
-            await botClient.SendTextMessageAsync(update.Message.Chat.Id, $"{storedClient.Data.BankName} - {storedClient.Data.Currency} {DateTime.Now.ToString("dd-mm-yyyy")}");
-            await botClient.SendTextMessageAsync(update.Message.Chat.Id, "Курс: template");
+            await botClient.SendTextMessageAsync(update.Message.Chat.Id, $"{storedClient.Data.BankName} - {storedClient.Data.Currency} {DateTime.Now.ToString("dd-MM-yyyy")}");
+            await botClient.SendTextMessageAsync(update.Message.Chat.Id, $"Курс: {(curs == 0 ? "Курс недоступен" : curs)}");
             storedClient.Action = Enums.BankWorker.Rate;
             return false;
         }

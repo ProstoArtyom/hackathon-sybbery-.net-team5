75cc2b9f5fefbc23e78a5a01427ba1d715883606
Kakelys <alexglobal1459@gmail.com>
2024-02-03 17:08:21 +0300

    fast crash fixes)

--- TgBot/Handler/CurrentDayHandler.cs
+++ TgBot/Handler/CurrentDayHandler.cs
@@ -22,16 +22,22 @@ namespace TgBot.Handler
         {
             var storedClient = LastActionStorage.Storage[update.Message.Chat.Id];
             decimal curs = 0;
-            switch(storedClient.Data.BankName)
+            try
             {
-                case "АльфаБанк":
-                    curs = (await GetInfo.GetRanks()).FirstOrDefault()?.SellRate ?? 0;
-                    break;
-                case "НБРБ":
-                    curs = (decimal)(await GetInfo.GetBBRates(currs[storedClient.Data.Currency])).Cur_OfficialRate;
-                    break;
+                switch(storedClient.Data.BankName)
+                {
+                    case "АльфаБанк":
+                        curs = (await GetInfo.GetRanks()).FirstOrDefault()?.SellRate ?? 0;
+                        break;
+                    case "НБРБ":
+                        curs = (decimal)(await GetInfo.GetBBRates(currs[storedClient.Data.Currency])).Cur_OfficialRate;
+                        break;
+                }
             }
-            
+            catch(Exception ex)
+            {
+                Console.WriteLine($"Failed to get currency for {storedClient.Data.BankName}:{storedClient.Data.Currency}; {ex.Message}");
+            }
 
             await botClient.SendTextMessageAsync(update.Message.Chat.Id, $"{storedClient.Data.BankName} - {storedClient.Data.Currency} {DateTime.Now.ToString("dd-MM-yyyy")}");
             await botClient.SendTextMessageAsync(update.Message.Chat.Id, $"Курс: {(curs == 0 ? "Курс недоступен" : curs)}");
--- TgBot/Program.cs
+++ TgBot/Program.cs
@@ -1,32 +1,42 @@
+using Microsoft.AspNetCore.Hosting;
 using Telegram.Bot;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types.Enums;
 using TgBot.Handler;
 using TgBot.Utils;
 
-var bot = new TelegramBotClient("6925511900:AAFIkCLTgwRyPcS27u2avPs6GainQHHduao");
-
-async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+start:
+try 
 {
-    Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(exception));
+    var bot = new TelegramBotClient("6925511900:AAFIkCLTgwRyPcS27u2avPs6GainQHHduao");
+
+    async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+    {
+        Console.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(exception));
+    }
+
+    var cts = new CancellationTokenSource();
+    var cancellationToken = cts.Token;
+    var receiverOptions = new ReceiverOptions
+    {
+        AllowedUpdates = new UpdateType[]{UpdateType.Message}, // only recieve new messages
+    };
+
+    bot.StartReceiving(
+        BaseHandler.HandleUpdateAsync,
+        HandleErrorAsync,
+        receiverOptions,
+        cancellationToken
+    );
+
+    Console.WriteLine("Press enter to exit");
+    Console.ReadLine();
+
+    Console.WriteLine("Saving data to file");
+    await LastActionStorage.SaveToFile();
 }
-
-var cts = new CancellationTokenSource();
-var cancellationToken = cts.Token;
-var receiverOptions = new ReceiverOptions
+catch(Exception ex)
 {
-    AllowedUpdates = new UpdateType[]{UpdateType.Message}, // only recieve new messages
-};
-
-bot.StartReceiving(
-    BaseHandler.HandleUpdateAsync,
-    HandleErrorAsync,
-    receiverOptions,
-    cancellationToken
-);
-
-Console.WriteLine("Press enter to exit");
-Console.ReadLine();
-
-Console.WriteLine("Saving data to file");
-await LastActionStorage.SaveToFile();
+    Console.WriteLine(ex);
+    goto start;
+}
